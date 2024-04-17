2609ed151e2135be0691bd17dcf7baaa7f84f534
traccoenlea <leatraccoen@gmail.com>
2024-04-17 15:48:22 +0200

    changed db access

--- routes/api/story.js
+++ routes/api/story.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 
-// router.get("/allStory", async (req, res) => {
-//   console.log("on est dans le back");
-//   const sql = `SELECT * FROM story`;
-//   connection.query(sql, (err, result) => {
-//     if (err) throw err;
-//     res.send(JSON.stringify(result));
-//   });
-// });
+router.get("/allStory", async (req, res) => {
+  console.log("on est dans le back");
+  const sql = `SELECT * FROM story`;
+  connection.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send(JSON.stringify(result));
+  });
+});
 
 module.exports = router;
